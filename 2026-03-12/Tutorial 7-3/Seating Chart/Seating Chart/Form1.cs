eb8be58ae0e0cef20a1358cc96904b2ad6947731
a114221095-commits <a114221095@mail.shu.edu.tw>
2026-03-26 14:54:24 +0800

    03_26

--- 2026-03-12/Tutorial 7-3/Seating Chart/Seating Chart/Form1.cs
+++ 2026-03-12/Tutorial 7-3/Seating Chart/Seating Chart/Form1.cs
@@ -19,8 +19,39 @@ namespace Seating_Chart
 
         private void displayPriceButton_Click(object sender, EventArgs e)
         {
-           
+            const int MAX_ROWS = 5;
+            const int MAX_COLS = 4;
+            int row, col;
+
+            decimal[,] seatPrices = { {450m, 450m, 450m, 450m},
+            {425m, 425m, 425m, 425m},
+            {400m, 400m, 400m, 400m},
+            {375m, 375m, 375m, 375m},
+            {350m, 350m, 350m, 350m}
+            };
+            if (int.TryParse(rowTextBox.Text, out row) && ((row - 1) < MAX_ROWS) && (row > 0))
+            {
+                if (int.TryParse(rowTextBox.Text, out col) && (((col - 1) < MAX_COLS) && (col > 0)))
+                {
+                    priceLabel.Text = seatPrices[row - 1, col - 1].ToString("C");
+                }
+
+
+
+                else
+                {
+                    MessageBox.Show("请输入有效的行数");
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("请输入有效的行数");
+                return;
+            }
         }
+
+
 
         private void exitButton_Click(object sender, EventArgs e)
         {
